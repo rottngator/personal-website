9a041961ae74af8d7f5755f34bc373846430712e
gator <lunruh946@gmail.com>
2025-09-28 22:15:30 -0500

    update
    added update section

---
+++ front-end/src/component/updates/update.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./update.css";
+
+const Update = () => {
+    return (
+        <div className="update-box">
+            <h2 className="update-title">UPDATES</h2>
+        </div>
+    );
+};   
+
+export default Update;
--- front-end/src/pages/home/home.js
+++ front-end/src/pages/home/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './home.css';
 import Nav from '../../component/nav/nav';
 import Footer from '../../component/footer/footer';
+import Update from '../../component/updates/update';
 
 const Home = () => {
     return (
@@ -11,15 +12,21 @@ const Home = () => {
         
         <img src='./images/decor/hikaru_yoshiki_header1.jpg' alt='header'></img>
         <Nav />
+        <Update />
     </div>
     <div class="status-section">
     <div className='status-img-section'>
             <img src='./images/decor/hikaruhome1.gif' alt='hikaru home'></img>
         </div>
-        
+    <p>test</p>
 
     </div>
-    <div class="content-section"></div>
+    <div class="content-section">
+        <div class="content-section-one">
+            <p>test</p>
+        </div>
+    </div>
+        
     
     <div class="footer-section">
         <Footer />
