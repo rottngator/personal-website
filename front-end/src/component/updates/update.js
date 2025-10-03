84198a89587ec46bbbbf1f1bcebe79b47adbdfc1
gator <lunruh946@gmail.com>
2025-10-03 16:30:46 -0500

    about updating

--- front-end/src/component/updates/update.js
+++ front-end/src/component/updates/update.js
@@ -7,6 +7,8 @@ const Update = () => {
             <h2 className="update-title">UPDATES    mm/dd/yyyy</h2>
             <p className="update-content">
                 <b>09/28/2025:</b> overhaul started
+                <br></br>
+                <b>09/29/2025:</b> about started
             </p>
         </div>
     );
--- front-end/src/pages/about/about.js
+++ front-end/src/pages/about/about.js
@@ -8,72 +8,72 @@ import Update from '../../component/updates/update';
 const About = () => {
     return (
         <div class="container">
-    <div class="main-container"></div>
-    <div class="header-section">
-        
-        <img src='./images/decor/hikaru_yoshiki_header1.jpg' alt='header'></img>
-        <Nav />
-        <Update />
-    </div>
-    
-    <div class="status-section">
-    <p>
-        <i>"Remember that you must die,"</i>
-        <br></br>
-        <i>"Remember that you must <b>live</b>."</i>
-    </p>
-    <div className='status-img-section'>
-            <img src='./images/backgrounds/hikaru_header2.jpg' alt='header'/>
+            <div class="main-container"></div>
+            <div class="header-section">
+                
+                <img src='./images/decor/hikaru_yoshiki_header1.jpg' alt='header'></img>
+                <Nav />
+                <Update />
+            </div>
             
+            <div class="status-section">
+                <p>
+                    <i>"Remember that you must die,"</i>
+                    <br></br>
+                    <i>"Remember that you must <b>live</b>."</i>
+                </p>
+                <div className='status-img-section'>
+                    <img src='./images/backgrounds/hikaru_header2.jpg' alt='header'/>
+                    
+                </div>
+            </div>
+            <div class="content-section">
+                <div class="content-section-one">
+                    
+                    <div className='about-icons-wrapper'> 
+                        <img className='about-icon' src='./images/about/hikaru_icon1.jpg' alt='icon' />
+                        <br></br>
+                        <img className='fragile-describer' src='./images/decor/about/fragile.gif' alt='fragile' />
+                        <img className='entity-describer' src='./images/decor/about/entity.gif' alt='entity' />
+                        <img className='flag-icon' src='./images/decor/about/demi_flag.gif' alt='demisexual flag' />
+                    </div>
+
+                    <ul className='about-me-list'>
+                        <li>
+                            <b>NAME:</b> gator
+                        </li>
+                        <li>
+                            <b>NICKNAMES:</b> swamp / thing / fish
+                        </li>
+                        <li>
+                            <b>AGE:</b> 21
+                        </li>
+                        <li>
+                            <b>PRONOUNS:</b> she / they / any
+                        </li>
+                        <li>
+                            <b>TIMEZONE:</b> CST
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="content-section-two">
+                    <p>
+                    heya, i'm some dork from the midwest who happens to know a little 
+                    coding ( i went to college for it ). 
+                    </p>
+
+                    <p>
+
+                    </p>
+                </div>
+            </div>
+            
+            
+            <div class="footer-section">
+                <Footer />
+            </div>
         </div>
-
-    </div>
-    <div class="content-section">
-        <div class="content-section-one">
-            <p>
-            <img className='about-icon' src='./images/about/hikaru_icon1.jpg' alt='icon' />
-            <img className='fragile-describer' src='./images/decor/fragile.gif' alt='fragile' />
-            <img className='flag-icon' src='./images/decor/about/demi_flag.gif' alt='demisexual flag' />
-            </p>
-
-            <ul className='about-me-list'>
-                <li>
-                    NAME: gator
-                </li>
-                <li>
-                    NICKNAMES: swamp / thing / fish
-                </li>
-                <li>
-                    AGE: 21
-                </li>
-                <li>
-                    PRONOUNS: she / they / any
-                </li>
-                <li>
-                    TIMEZONE: CST
-                </li>
-            </ul>
-
-
-        </div>
-        <div class="content-section-two">
-            <p>
-            hello, i currently only speak english but i want to seriously start 
-            learning german and japanese ( so when i do have the money to travel, i 
-            won't have to rely as heavily on translaters ). 
-            </p>
-
-            <p>
-
-            </p>
-        </div>
-    </div>
-        
-    
-    <div class="footer-section">
-        <Footer />
-    </div>
-</div>
     );
 }
 
